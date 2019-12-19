76011ebc5f05eeeb9025fb709490ac555d50768a
Jumar <mdjumaralam@gmail.com>
2019-12-19 23:39:22 +0600

    Documentation added for lostandfound model file

--- StudentAssistant/v1/modelLostNFound/lostnfound.py
+++ StudentAssistant/v1/modelLostNFound/lostnfound.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.db.models import Max
 
 class LostandFound(models.Model):
+	"""
+	LostandFound class
+	Attributes: lost_id, finders_id, itemtype, loser_id, finder_contact_email, lost_item, status
+	"""
     lost_id = models.AutoField(unique=True, primary_key= True)
     finders_id = models.IntegerField(null= False)
     itemtype = models.CharField(max_length=30)
