2c3e46a6614159098059b75c4e9b9e63a731d96e
Jumar <mdjumaralam@gmail.com>
2019-12-19 23:47:02 +0600

    Documentation added for forms.py

--- StudentAssistant/v1/forms.py
+++ StudentAssistant/v1/forms.py
@@ -1,4 +1,9 @@
 class LoginForm(forms.Form):
+"""
+	LoginForm class
+	Attributes: user_id, password
+	This class takes the user_id and password from frontend form and pass them to models
+"""
     user_id = forms.IntegerField(
         widget=forms.TextInput(attrs={'type':'int', 'class': "form-control", 'placeholder': 'University ID'}),
         label="University ID")
@@ -7,7 +12,11 @@ class LoginForm(forms.Form):
 
 
 class gradeForm(forms.Form):
-
+"""
+	GradeForm class
+	Attributes: courseName, courseGrade, semester
+	This class takes the courseName, courseGrade and semester from frontend form. After calculating the grades in models it passes the data to database.
+"""
     c = [('1','A'),('2','A-'),('3','B+'),('4','B'),('5','B-'),('6','C+'),('7','C'),('8','C-'),('10','D+'),('9','D') ]
     courseName = forms.CharField(max_length=8, widget=forms.TextInput(attrs={'type':'text','class':"form-control",'placeholder':'Course Name'}), label='Course')
     courseGrade = forms.ChoiceField(choices=c,label='Grades',widget=forms.Select(attrs={'class':"form-control",'placeholder':'Course Grade'}))
@@ -17,6 +26,11 @@ class gradeForm(forms.Form):
 
 
 class LostForm(forms.Form):
+"""
+	LostForm class
+	Attributes: item, itemtype, loser_id, description, contactInfo
+	This class takes the citem, itemtype, loser_id, description and contactInfo from frontend form.
+"""
     item = [('1', 'ID_Card'), ('2', 'Pen-Drive'), ('3','others')]
     itemtype = forms.ChoiceField(choices=item, label='Item' ,widget=forms.Select(attrs={'class':"form-control",'placeholder':'Item'}))
     loser_name = forms.CharField( label='Name(if available)' ,widget=forms.Select(attrs={'class':"form-control",'placeholder':'Name'}))
