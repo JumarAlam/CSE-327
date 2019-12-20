67c1e831efeb508425f985b2fab7b5c750ae3f86
Shahriar-Akash <shariar.akash@northsouth.edu>
2019-12-20 19:50:00 +0600

    firms issue fixed

--- StudentAssistant/forms.py
+++ StudentAssistant/forms.py
@@ -4,8 +4,11 @@ from django import forms
     generating form 
     '''
 
-class ComplaintForm(forms.Form): 
-    fullname = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':"form-control"}),label='Full Name')
-    email = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':"form-control"}),label='Email Address')
-    comment = forms.CharField(max_length=1000, widget=forms.TextInput(attrs={'class':"form-control"}), label='Comment')
 
+class ComplaintForm(forms.Form):
+    fullname = forms.CharField(max_length=50, widget=forms.TextInput(
+        attrs={'class': "form-control"}), label='Full Name')
+    email = forms.CharField(max_length=50, widget=forms.TextInput(
+        attrs={'class': "form-control"}), label='Email Address')
+    comment = forms.CharField(max_length=1000, widget=forms.TextInput(
+        attrs={'class': "form-control"}), label='Comment')
