cd594bd4bb7e249f5f7f32794dd389bd0589c7c5
Tanjeeb-Alam <tanjeeb.alam@northsouth.edu>
2019-12-21 00:07:36 +0600

    Create models.py

---
+++ StudentAssistant/v1/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.db.models import Max
+'''
+Each Courses are either from SEPS or University General Education or from CSE core courses 
+and a student must have to maintain a specific CGPA in these category in the end of the degree
+'''
