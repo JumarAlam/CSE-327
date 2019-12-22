9ccddfe2b978a2f704738b415cd1a974bd792ac8
Shahriar-Akash <shariar.akash@northsouth.edu>
2019-12-22 20:26:09 +0600

    backup file added

---
+++ bfile/StudentAssistant/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+''' inherit complaintForm from 
+    generating form 
+    '''
+
+
+class ComplaintForm(forms.Form):
+    fullname = forms.CharField(max_length=50, widget=forms.TextInput(
+        attrs={'class': "form-control"}), label='Full Name')
+    email = forms.CharField(max_length=50, widget=forms.TextInput(
+        attrs={'class': "form-control"}), label='Email Address')
+    comment = forms.CharField(max_length=1000, widget=forms.TextInput(
+        attrs={'class': "form-control"}), label='Comment')
